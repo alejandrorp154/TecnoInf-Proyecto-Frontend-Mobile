9860ca9f7fb36e4cb70e4fc8fe44310155dc8a5e
Fernando Madriaga <nandomadriaga@gmail.com>
2021-04-07 11:36:27 -0300

    Corregido

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistrarComponent } from './registrar/registrar.component';
 
 const routes: Routes = [
   {
