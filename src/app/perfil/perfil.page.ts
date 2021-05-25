07eca96268c89b52673e523a67c98b27f4c4abcd
Dario Andres Gomez Cedres <dario.andres.gomez@fing.edu.uy>
2021-05-25 20:45:46 -0300

    Arreglo luego de marge Alejandro

--- src/app/perfil/perfil.page.ts
+++ src/app/perfil/perfil.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-perfil',
@@ -12,5 +12,6 @@ export class PerfilPage implements OnInit {
 
   ngOnInit() {
   }
+
 
 }
