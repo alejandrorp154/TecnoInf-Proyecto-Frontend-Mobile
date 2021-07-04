47966bab7f7db7df6ff4385d53f2ab1dfe7ccfda
Gomez13XD <andresgomezcedres@hotmail.com>
2021-07-03 21:58:42 -0300

    Cambio en enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,16 @@
 export const environment = {
   production: true,
-  firebaseAPIKey: 'AIzaSyASAHOH5sSKN814Ma1OILygy0_9bICC4iI'
+  firebaseAPIKey: 'AIzaSyASAHOH5sSKN814Ma1OILygy0_9bICC4iI',
+  firebaseConfig: {
+    apiKey: "AIzaSyASAHOH5sSKN814Ma1OILygy0_9bICC4iI",
+    authDomain: "proyecto-ab052.firebaseapp.com",
+    databaseURL: "https://proyecto-ab052-default-rtdb.firebaseio.com",
+    projectId: "proyecto-ab052",
+    storageBucket: "proyecto-ab052.appspot.com",
+    messagingSenderId: "217144319364",
+    appId: "1:217144319364:web:9843c286af3eb9aa7351d1",
+    measurementId: "G-2BR0ECZ1KS"
+
+  },
+  mapboxKey: 'pk.eyJ1IjoidHJhdmVscGFjazIwMjEiLCJhIjoiY2tuNDR0cjl4MWUwbDJwbzgwcWY2NTRieSJ9.Fju2qmaYyp6zHcXCClCifg'
 };
