a17daffd4fb613e6ca9f2c6b6de60de5efaa1147
Gomez13XD <andresgomezcedres@hotmail.com>
2021-07-29 20:32:18 -0300

    https

--- src/app/servicios/link-prev.service.ts
+++ src/app/servicios/link-prev.service.ts
@@ -9,6 +9,6 @@ export class LinkPrevService {
   constructor(public httpClient: HttpClient) { }
 
   getPreview (url: string){
-    return this.httpClient.get('http://api.linkpreview.net/?key=3f5c001d3973bdc74456aea7e1464340&q='+url);
+    return this.httpClient.get('https://api.linkpreview.net/?key=3f5c001d3973bdc74456aea7e1464340&q='+url);
   }
 }
